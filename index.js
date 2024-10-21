faa0ea0d72bcefe87c69ecd4323177e158e604ba
PadBro <64410553+PadBro@users.noreply.github.com>
2024-10-21 10:08:27 +0200

    Add eslint + prettier (#3)
    * Add eslint + prettier
    
    * fix linter

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const { EmbedBuilder } = require('discord.js');
-const dayjs = require('dayjs')
-const relativeTime = require("dayjs/plugin/relativeTime");
-const duration = require("dayjs/plugin/duration");
+const dayjs = require('dayjs');
+const relativeTime = require('dayjs/plugin/relativeTime');
+const duration = require('dayjs/plugin/duration');
 
 dayjs.extend(duration);
 dayjs.extend(relativeTime);
@@ -20,32 +20,32 @@ const client = new Client({ intents: [intents] });
 const dotenv = require('dotenv');
 dotenv.config();
 
-client.once(Events.ClientReady, readyClient => {
+client.once(Events.ClientReady, (readyClient) => {
   console.log(`Ready! Logged in as ${readyClient.user.tag}`);
 });
 
-client.on("guildMemberRemove", member => {
+client.on('guildMemberRemove', (member) => {
   const channel = client.channels.cache.get(process.env.LEAVE_CHANNEL);
-  sendLeaveMessage(channel, member)
+  sendLeaveMessage(channel, member);
 });
 
 client.login(process.env.DISCORD_TOKEN);
 
 const sendLeaveMessage = (channel, member) => {
-
   const roles = member.roles.cache;
   const rolesString = roles.map((role) => role.name);
-  const rolesStringFiltered = rolesString.filter((role) => role != '@everyone')
+  const rolesStringFiltered = rolesString.filter((role) => role != '@everyone');
 
-
-  let embedColor = 'FF0000'
-  if (roles.find(role => role.id == process.env.MEMBER_ROLE_ID)) {
-    const memberRole = member.guild.roles.cache.find(role => role.id == process.env.MEMBER_ROLE_ID)
-    embedColor = memberRole.hexColor
+  let embedColor = 'FF0000';
+  if (roles.find((role) => role.id == process.env.MEMBER_ROLE_ID)) {
+    const memberRole = member.guild.roles.cache.find(
+      (role) => role.id == process.env.MEMBER_ROLE_ID,
+    );
+    embedColor = memberRole.hexColor;
   }
 
-  const diff = dayjs().diff(dayjs(member.joinedAt), 'day')
-  const duration = dayjs.duration(diff, "days").humanize();
+  const diff = dayjs().diff(dayjs(member.joinedAt), 'day');
+  const duration = dayjs.duration(diff, 'days').humanize();
 
   const embed = new EmbedBuilder()
     .setColor(embedColor)
@@ -55,10 +55,16 @@ const sendLeaveMessage = (channel, member) => {
       { name: 'Nickname:', value: `${member.nickname ?? '---'}`, inline: true },
       { name: 'Displayname:', value: `${member.displayName}`, inline: true },
       { name: '\u200B', value: '\u200B' },
-      { name: 'Roles:', value: rolesStringFiltered.length ? rolesStringFiltered.join(', ') : '---', inline: true },
+      {
+        name: 'Roles:',
+        value: rolesStringFiltered.length
+          ? rolesStringFiltered.join(', ')
+          : '---',
+        inline: true,
+      },
       { name: 'Time on Server', value: `${duration}`, inline: true },
     )
     .setTimestamp();
 
   channel.send({ embeds: [embed] });
-}
+};
