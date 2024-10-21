f830c192159b67c40704bdd69e9eee4266425d66
PadBro <64410553+PadBro@users.noreply.github.com>
2024-10-21 17:33:21 +0200

    Add role on join (#4)
    * Add role on join
    
    * fix linter

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 const { Client, Events, IntentsBitField } = require('discord.js');
 const { sendLeaveMessage } = require('./utils/sendLeaveMessage');
+const { addRole } = require('./utils/addRole');
+const dotenv = require('dotenv');
+dotenv.config();
 
 const flags = [
   IntentsBitField.Flags.Guilds,
   IntentsBitField.Flags.GuildMembers,
   IntentsBitField.Flags.GuildPresences,
 ];
+
 const intents = new IntentsBitField();
 intents.add(flags);
 const client = new Client({ intents: [intents] });
-
-const dotenv = require('dotenv');
-dotenv.config();
 
 client.once(Events.ClientReady, (readyClient) => {
   console.log(`Ready! Logged in as ${readyClient.user.tag}`);
@@ -20,6 +21,12 @@ client.once(Events.ClientReady, (readyClient) => {
 client.on('guildMemberRemove', (member) => {
   const channel = client.channels.cache.get(process.env.LEAVE_CHANNEL);
   sendLeaveMessage(channel, member);
+});
+
+client.on('guildMemberAdd', (member) => {
+  if (process.env.JOIN_ROLE_ID) {
+    addRole(member, process.env.JOIN_ROLE_ID);
+  }
 });
 
 client.login(process.env.DISCORD_TOKEN);
