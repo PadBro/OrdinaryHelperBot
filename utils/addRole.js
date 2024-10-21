8d0e2d8b8d20cd0b82c119e83b4d892438b9d683
PadBro <padmaster2012@googlemail.com>
2024-10-21 17:30:53 +0200

    Add role on join

---
+++ utils/addRole.js
@@ -0,0 +1,9 @@
+const addRole = (member, role_id) => {
+    try {
+      member.roles.add(role_id)
+    } catch(e) {
+      console.log(`could not assign role on memberAdd: ${e}`);
+    }
+}
+
+module.exports = { addRole }
