bc412dda5a00d1d9d782840d9e97463c386b21fa
PadBro <64410553+PadBro@users.noreply.github.com>
2024-10-23 14:58:32 +0200

    Add faq command (#7)
    * Add faq command
    
    * improve code

---
+++ commands/utils/faq.js
@@ -0,0 +1,38 @@
+const { SlashCommandBuilder } = require('discord.js');
+const dotenv = require('dotenv');
+dotenv.config();
+
+let faqs = [];
+try {
+  faqs = require('../../configs/faq.json');
+} catch (e) {
+  console.log('[WARNING] No FAQs found.');
+}
+
+const choices = faqs.map((faq) => ({ name: faq.name, value: faq.key }));
+
+const command = {
+  data: new SlashCommandBuilder()
+    .setName('faq')
+    .setDescription('Answeres frequently asked questions')
+    .addStringOption((option) =>
+      option
+        .setName('category')
+        .setDescription('The FAQ category')
+        .setRequired(true)
+        .addChoices(choices),
+    ),
+
+  async execute(interaction) {
+    const category = interaction.options.getString('category');
+    const faq = faqs.find((faq) => faq.key === category);
+    if (!faq) {
+      await interaction.reply(
+        'The quiestion was not found please try again later. If this error pressists please report to the staff team.',
+      );
+    } else {
+      await interaction.reply(faq.answere);
+    }
+  },
+};
+module.exports = faqs.length ? command : {};
---
+++ utils/deploy-commands.js
@@ -0,0 +1,32 @@
+const { REST, Routes } = require('discord.js');
+const dotenv = require('dotenv');
+const { getCommands } = require('./getCommands');
+dotenv.config();
+
+const deployCommands = async () => {
+  const commands = getCommands().map((command) => command.data.toJSON());
+
+  const rest = new REST().setToken(process.env.DISCORD_TOKEN);
+
+  try {
+    console.log(
+      `Started refreshing ${commands.length} application (/) commands.`,
+    );
+
+    const data = await rest.put(
+      Routes.applicationGuildCommands(
+        process.env.CLIENT_ID,
+        process.env.GUILD_ID,
+      ),
+      { body: commands },
+    );
+
+    console.log(
+      `Successfully reloaded ${data.length} application (/) commands.`,
+    );
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+module.exports = { deployCommands };
