00d5a7f3fad65eff16011014c313078d3c2b1235
PadBro <padmaster2012@googlemail.com>
2024-11-02 00:18:42 +0100

    improve code

--- utils/interactionHandler/modal.js
+++ utils/interactionHandler/modal.js
@@ -12,12 +12,12 @@ export const handler = async (interaction) => {
     console.error(error);
     if (interaction.replied || interaction.deferred) {
       await interaction.followUp({
-        content: 'There was an error while executing this command!',
+        content: 'There was an error while handling the modal!',
         ephemeral: true,
       });
     } else {
       await interaction.reply({
-        content: 'There was an error while executing this command!',
+        content: 'There was an error while handling the modal!',
         ephemeral: true,
       });
     }
