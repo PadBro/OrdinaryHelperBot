aa50a9a34aa09e0730398e41fcf76ea10d5144ce
PadBro <padmaster2012@googlemail.com>
2024-11-06 23:09:05 +0100

    add ip command

---
+++ commands/ip.js
@@ -0,0 +1,13 @@
+import { SlashCommandBuilder, PermissionFlagsBits } from 'discord.js';
+import fetch from 'node-fetch';
+
+export const data = new SlashCommandBuilder()
+  .setName('ip')
+  .setDescription('Get the bots ip address')
+  .setDefaultMemberPermissions(PermissionFlagsBits.KickMembers);
+
+export const execute = async (interaction) => {
+  const response = await fetch('https://api.ipify.org/');
+  const ip = await response.text();
+  await interaction.reply('Current ip: `' + ip + '`');
+};
