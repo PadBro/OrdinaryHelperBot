2282ff2f436ad113922373642ff2b2edf37d46fc
GoldenKoopa <100142836+GoldenKoopa@users.noreply.github.com>
2024-11-15 21:21:45 +0100

    fix linter

--- commands/utils/purge/executePurge.js
+++ commands/utils/purge/executePurge.js
@@ -21,7 +21,9 @@ export const executePurge = async (interaction) => {
   }
 
   const filteredMembers = await removeMemberRoles(interaction);
-  const members = filteredMembers?.map((member) => `${member.nickname || member.displayName}`);
+  const members = filteredMembers?.map(
+    (member) => `${member.nickname || member.displayName}`
+  );
 
   const embed = createEmbed(members);
   embed.setDescription(`${members.length} members purged`);
