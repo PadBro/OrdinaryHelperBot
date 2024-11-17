90b9b3aaa9904bc59a04e3ff7b03b899faf41d79
PadBro <padmaster2012@googlemail.com>
2024-11-17 23:02:43 +0100

    Update database.js

--- utils/database.js
+++ utils/database.js
@@ -5,6 +5,7 @@ let options = {
   host: process.env.DB_HOST,
   port: process.env.DB_PORT ?? 3306,
   dialect: 'mariadb',
+  logging: process.env.ENVIRONMENT !== 'prod' ? Logger.debug : false,
 };
 
 if (process.env.ENVIRONMENT === 'test') {
@@ -18,8 +19,5 @@ export const sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
   process.env.DB_PASSWORD,
-  {
-    ...options,
-    logging: process.env.ENVIRONMENT !== 'prod' ? Logger.debug : false,
-  }
+  options,
 );
