49939c10ff7c3c899ed0dfdb92115aea1e79654c
PadBro <padmaster2012@googlemail.com>
2024-11-17 22:09:33 +0100

    test workflow

--- utils/database.js
+++ utils/database.js
@@ -1,24 +1,28 @@
 import { Sequelize } from 'sequelize';
 import Logger from './logger.js';
 
-console.log(process.env.DB_NAME)
-console.log(process.env.DB_USER)
-console.log(process.env.DB_PASSWORD)
-console.log(process.env.DB_HOST)
-console.log(process.env.DB_PORT)
+
+let options = {
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT ?? 3306,
+  dialect: 'mariadb',
+}
+
+console.log(process.env.ENVIRONMENT)
+
+if (process.env.ENVIRONMENT === 'test') {
+  options = {
+    dialect: 'sqlite',
+    storage: './database.sqlite'
+  }
+}
 
 export const sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
   process.env.DB_PASSWORD,
   {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT ?? 3306,
-    dialect: 'mariadb',
-    dialectOptions: {
-        socketPath: "/var/run/mysqld/mysqld.sock",
-        allowPublicKeyRetrieval: true
-    },
+    ...options,
     logging: process.env.ENVIRONMENT !== 'prod' ? Logger.debug : false,
   }
 );
