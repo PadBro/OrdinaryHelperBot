8bce2b6c97abe9183090dba8ded4bccafcdf3e16
PadBro <padmaster2012@googlemail.com>
2024-11-17 22:14:01 +0100

    Update setup.js

--- tests/setup.js
+++ tests/setup.js
@@ -1,8 +1,8 @@
-import { beforeEach } from 'vitest';
+import { beforeAll } from 'vitest';
 import { sequelize } from '../utils/database.js';
 import models from '../models/index.js';
 
-beforeEach(async () => {
+beforeAll(async () => {
   await sequelize.authenticate();
   for (const model of models) {
     await model.sync({ force: true });
