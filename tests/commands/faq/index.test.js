b21693c1d038a3177c00e0abbddeb84d56533aa4
PadBro <padmaster2012@googlemail.com>
2024-11-17 18:32:24 +0100

    fix linter

--- tests/commands/faq/index.test.js
+++ tests/commands/faq/index.test.js
@@ -1,35 +1,35 @@
-import { expect, test, beforeEach, vi } from 'vitest'
-import { autocomplete } from "../../../commands/faq/index.js"
-import { faq } from "../../../models/faq.js"
+import { expect, test, beforeEach, vi } from 'vitest';
+import { autocomplete } from '../../../commands/faq/index.js';
+import { faq } from '../../../models/faq.js';
 
 const interaction = {
   options: {
-    getFocused: vi.fn()
+    getFocused: vi.fn(),
   },
-  respond: vi.fn()
-}
+  respond: vi.fn(),
+};
 
 beforeEach(async () => {
   await faq.bulkCreate([
     {
       id: 1,
       question: 'Test',
-      answer: 'Testing'
+      answer: 'Testing',
     },
     {
       id: 2,
       question: 'Abc',
-      answer: 'Def'
-    }
-  ])
-})
+      answer: 'Def',
+    },
+  ]);
+});
 
 test('can retrive autocomplete', async () => {
-  interaction.options.getFocused.mockReturnValue('')
+  interaction.options.getFocused.mockReturnValue('');
   await autocomplete(interaction);
 
   expect(interaction.respond).toBeCalledWith([
     { name: 'Test', value: '1' },
     { name: 'Abc', value: '2' },
-  ])
+  ]);
 });
