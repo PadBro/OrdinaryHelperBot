f03fd85b167011efa1ee731449809ba743715659
PadBro <64410553+PadBro@users.noreply.github.com>
2024-11-22 01:43:18 +0100

    add add alt account (#28)
    * add add alt account
    
    * remove timestamp
    
    * Update package-lock.json
    
    * Update .env.example
    
    * improve code

---
+++ commands/addAltAccount.js
@@ -0,0 +1,46 @@
+import {
+  SlashCommandBuilder,
+  PermissionFlagsBits,
+  EmbedBuilder,
+} from 'discord.js';
+import Logger from '../utils/logger.js';
+
+export const data = new SlashCommandBuilder()
+  .setName('add-alt-account')
+  .setDescription('Adds an alt account')
+  .setDefaultMemberPermissions(PermissionFlagsBits.ViewAuditLog)
+  .addUserOption((option) =>
+    option
+      .setName('member')
+      .setDescription('The member to add an alt account')
+      .setRequired(true)
+  )
+  .addUserOption((option) =>
+    option.setName('alt').setDescription('The alt account').setRequired(true)
+  );
+
+export const execute = async (interaction) => {
+  const member = interaction.options.getUser('member');
+  const alt = interaction.options.getUser('alt');
+
+  const embed = new EmbedBuilder()
+    .setColor('#f0833a')
+    .setDescription(`${member} - ${alt}`);
+
+  try {
+    await interaction.channel.send({ embeds: [embed] });
+  } catch (e) {
+    Logger.error(`Could not add alt account: ${e}`);
+    interaction.reply({
+      content:
+        'An error occurred while adding the alt account. Please try again later. If this error persists, please report to the staff team.',
+      ephemeral: true,
+    });
+    return;
+  }
+
+  interaction.reply({
+    content: 'Alt account was added',
+    ephemeral: true,
+  });
+};
